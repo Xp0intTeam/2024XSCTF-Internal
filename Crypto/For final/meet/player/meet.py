94de53f2e8dcb1b8683c7e64e3a803a29689896f
HvAng <142745026+HvAng10@users.noreply.github.com>
2024-10-21 20:41:46 +0800

    Add files via upload

--- Crypto/For final/meet/player/meet.py
+++ Crypto/For final/meet/player/meet.py
@@ -26,3 +26,7 @@ key = Key_Gen(chars, 3)
 cipher = encrypt(flag, key)
 print(f'c = {cipher[0].hex()}')
 print(f'enc_flag = {cipher[1].hex()}')
+"""
+c = 3f9b376d07e5f6633ed23ca74a502fbe
+enc_flag = 641ec79497a475265aa424fe9d8df460ddd495991e92084a65063f0b08a04159547214a81038661557ee9da8685bc7e7e45f73936e3c3f2fad09181f2e33cf45
+"""
