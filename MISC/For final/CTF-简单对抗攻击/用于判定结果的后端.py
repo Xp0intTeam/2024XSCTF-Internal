1d9e2372a6f86b1091ccff76a3385c5f61260014
gbljdgb <88969496+gbljdgb@users.noreply.github.com>
2024-10-27 02:03:01 +0800

    Add files via upload

--- MISC/For final/CTF-简单对抗攻击/用于判定结果的后端.py
+++ MISC/For final/CTF-简单对抗攻击/用于判定结果的后端.py
@@ -11,17 +11,21 @@ from flask_limiter.util import get_remote_address
 app = Flask(__name__)
 limiter = Limiter(
     app,
-    key_func=get_remote_address,   # 根据访问者的IP记录访问次数
+    key_func=get_remote_address,  # 根据访问者的IP记录访问次数
     default_limits=["20 per minute"]  # 默认限制，一分钟最多访问20次
 )
 
 # Load model
 model = timm.create_model('resnet18', checkpoint_path="ckpt/pytorch_model.bin")
 model.eval()
+
+# 读取原图和设置原始标签
 ori_path = "figure/cat.png"
 ori_img = Image.open(ori_path).convert('RGB')
+ori_img_numpy = np.array(ori_img)
 ori_label = 282
 
+# 定义data aug
 data_transforms = transforms.Compose([
        transforms.ToTensor(),
        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -55,25 +59,42 @@ def upload_file():
         img = Image.open(file.stream)  # Use file.stream to read directly
         img.verify()  # Verify the image is valid
         img = Image.open(file.stream).convert('RGB')  # Reopen the image after verification
+        img_numpy = np.array(img)
     except Exception as e:
         return jsonify({'error': 'Invalid image file.'}), 400
 
     if np.array(ori_img).shape != np.array(img).shape:
         return jsonify({
-            '图像尺寸为': np.array(ori_img).shape,
-            'msg': "大小都和原图不一样, 不要欺骗我了555, 传张原图看看规则吧"
+            '原始图像的尺寸为': np.array(ori_img).shape,
+            'msg': "大小都和原图不一样, 这都想骗我？传张原图看看规则吧"
         })
     
-    s = ssim(np.array(ori_img), np.array(img), multichannel=True, channel_axis=2)
-    s *= 100. # 0~100, 越高越相似
-    mse = float(np.mean( (np.array(ori_img).astype('float32') - np.array(img).astype('float32')) ** 2 ))
+    def compute_patch_ssim(ori_img, img, patch_size=(28, 28)):
+        h, w, _ = ori_img.shape
+        ssim_values = []
+
+        for i in range(0, h, patch_size[0]):
+            for j in range(0, w, patch_size[1]):
+                ori_patch = ori_img[i:i+patch_size[0], j:j+patch_size[1]]
+                img_patch = img[i:i+patch_size[0], j:j+patch_size[1]]
+                
+                # 确保patch大小一致
+                if ori_patch.shape == img_patch.shape:
+                    ssim_value = ssim(ori_patch / 255.0, img_patch / 255.0, multichannel=True, channel_axis=2)
+                    ssim_values.append(ssim_value * 100)
+
+        return ssim_values
     
-    if s < 90 or mse > 1000:
+    s = compute_patch_ssim(ori_img_numpy, img_numpy)
+    
+    s_Threshold = 90
+    has_value_less_than_s_Threshold = any(value < s_Threshold for value in s)
+    
+    if has_value_less_than_s_Threshold:
         return jsonify({
-            '原始类别（任务目标1：目标类别≠预测类别）': ori_label,
+            '原始类别(任务目标1: 目标类别≠预测类别)': ori_label,
             '预测类别': '传张像点的嘛~, 你这样不给你预测了',
-            'ssim（任务目标2：SSIM>=90）': s,
-            'mse（任务目标3：mse<=1000）': mse,
+            f'SSIM(任务目标2: 图片被切割成64块28×28的切块, 并对每个切块计算SSIM, 要求每个切块原图与扰动图的SSIM >= {s_Threshold})': s,
         })
     
     # Process the image for prediction
@@ -83,18 +104,18 @@ def upload_file():
 
     if top5_class_indices.squeeze().tolist()[0] == ori_label:
         return jsonify({
-            '原始类别（任务目标1：目标类别≠预测类别）': ori_label,
+            '原始类别(任务目标1: 目标类别≠预测类别)': ori_label,
             '预测类别': top5_class_indices.squeeze().tolist()[0],
-            'ssim（任务目标2：SSIM>=90）': s,
-            'mse（任务目标3：mse<=1000）': mse,
+            f'SSIM(任务目标2: 图片被切割成64块28×28的切块, 并对每个切块计算SSIM, 要求每个切块原图与扰动图的SSIM >= {s_Threshold})': s,
+            '预测概率': top5_probabilities.squeeze().tolist()[0],
         })
     else:
         return jsonify({
-            '原始类别（任务目标1：目标类别≠预测类别）': ori_label,
+            '原始类别(任务目标1: 目标类别≠预测类别)': ori_label,
             '预测类别': top5_class_indices.squeeze().tolist()[0],
-            'ssim（任务目标2：SSIM>=90）': s,
-            'msg': "牛的，给你flag：XSCTF{You_R_New_NEW_Give_U_F1Ag_aNd_M1lkTea}",
-            'mse（任务目标3：mse<=1000）': mse,
+            f'SSIM(任务目标2: 图片被切割成64块28×28的切块, 并对每个切块计算SSIM, 要求每个切块原图与扰动图的SSIM >= {s_Threshold})': s,
+            'msg': "牛的, 给你flag: XSCTF{You_R_New_NEW_Give_U_F1Ag_aNd_M1lkTea}, 记得保存攻击成功的图片~",
+            '预测概率': top5_probabilities.squeeze().tolist()[0],
         })
 
 if __name__ == '__main__':
